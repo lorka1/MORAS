e65104e7b7bb55e08e50a845aa5cf488477a0f05
Lorena <134442286+lorka1@users.noreply.github.com>
2023-11-26 15:46:56 +0100

    Add files via upload

---
+++ zadaca_2/3zad.asm
@@ -0,0 +1,21 @@
+@0
+D = M // D = RAM[0]
+
+@1
+D = D + M // D = D + RAM[1]
+
+@2
+D = D + M // D = D + RAM[]
+
+@3
+D = D + M // D = D + RAM[3]
+
+@4
+D = D + M // D = D + RAM[4]
+
+@5
+M = D // RAM[5] = D
+
+(END)
+@END
+0; JMP
